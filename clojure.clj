0b8d6f93d96b442a54e75544dd66e0a841bb14ac
Renan Assunção <rhassuncao@hotmail.com>
2020-06-02 15:49:18 -0300

    initial commit

---
+++ clojure.clj
@@ -0,0 +1,12 @@
+(defn valor-descontado
+	"Retorna o valor com desconto de 10% se o valor bruto for estritamente maior que 100."
+	[valor-bruto]
+	(if (> valor-bruto 100)
+	(let [taxa-de-desconto (/ 10 100)
+	      desconto         (* valor-bruto taxa-de-desconto)]
+		(println "Calculando desnconto de" desconto)
+	(- valor-bruto desconto))
+	valor-bruto))
+
+(valor-descontado 100)
+		
